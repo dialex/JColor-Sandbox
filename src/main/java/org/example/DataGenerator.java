481d8288ea7b1e2a36bc2f204c94bca1e0c95b3d
Diogo Nunes <dialex.nunes@gmail.com>
2020-07-05 15:17:46 +0100

    use the latest JCDP, I mean... JColor :)

---
+++ src/main/java/org/example/DataGenerator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Random;
+
+public class DataGenerator {
+
+    public static final String NEWLINE = System.getProperty("line.separator");
+    private static final Random _rand = new Random();
+
+    public static String createText() {
+        return "Message";
+    }
+
+    public static String createTextWithId(int n) {
+        return "Message" + n;
+    }
+
+    public static String createTextLine() {
+        return "This is a line" + NEWLINE;
+    }
+
+    public static int randomInt(int upperLimit) {
+        return _rand.nextInt(upperLimit);
+    }
+}
